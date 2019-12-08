2b3106123fdf3a82a3f73fc05f5511d5e59977ab
syahminazari <57428253+syahminazari@users.noreply.github.com>
2019-12-08 22:28:00 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// to serve our JavaScript, CSS and index.html
+app.use(express.static('./'));
+
+var pusher = new Pusher({
+  appId: '908380',
+  key: '2b451bd87b4f8c62814d',
+  secret:  '44a62ecabee229c6f48d',
+  cluster: 'ap1'
+});
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Listening at http://localhost:5000'));
